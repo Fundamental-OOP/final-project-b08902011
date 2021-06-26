2063179aab6c430d5158718507645ff23c14e933
Ken30510 <ken30510@gmail.com>
2021-06-26 15:08:08 +0800

    test JFrame

---
+++ src/basic_object/Screen.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Screen{
+	private String name;
+	private JFrame frame = new JFrame();
+	public Screen(String name){
+		this.name = name;
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+}
---
+++ src/basic_object/Test.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Test{
+	public static void main(String[] args){
+		Screen map = new Screen("map");
+	}
+}
