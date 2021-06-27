97ee1e7803c2708ea64b7038213fb037a91b6b02
AndyDu01 <andy255060@gmail.com>
2021-06-27 18:13:04 +0800

    empty

--- src/Servant/NormalAttack.java
+++ src/Servant/NormalAttack.java
@@ -1,5 +1,15 @@
 package Servant;
-
-public class NormalAttack {
-    
-}
+import java.util.*;
+public class NormalAttack extends Skill{
+    // public int attackCycle = 0;
+    public NormalAttack(int nTarget, int range, int cd, boolean toenemy, boolean toally) {
+        super(nTarget, range, cd, toenemy, toally);
+        // this.attackCycle = attackCycle;
+    }
+    @Override
+    public void Act(Unit performer,Vector<Unit> victim){
+        for(Unit u:victim){
+            u.takeDamage(((Servant)performer).ATK());
+        }
+    }
+}                                         
